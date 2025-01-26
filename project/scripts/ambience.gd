dad94b1fbee9b168cc8eff35d0b2075861805fcd
JonathanHaws <jonathanmhaws1@gmail.com>
2025-01-26 15:50:32 -0700

    player textures, audio fixes, cutscene broken

--- project/scripts/ambience.gd
+++ project/scripts/ambience.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var ENTER_AREA: Area3D
 @export var EXIT_AREA: Area3D
 var inside = false
+var queued_free = false
 
 func _on_body_entered(body: Node) -> void:
 	if body != REAPER: return
@@ -17,15 +18,17 @@ func _on_body_exited(body: Node) -> void:
 	inside = false
 	if ANIM: ANIM.queue("EXITED")
 	
+func _on_animation_finished(_animation_name):
+	if ANIM.get_queue().size() == 0 and queued_free:
+		queue_free() 
+
 func _connect_exit_queue_free() -> void:
 	if ANIM: ANIM.queue("EXITED")
 	if ANIM:
 		ANIM.queue("EXITED")
 		if not ANIM.is_connected("animation_finished", Callable(self, "_on_animation_finished")):
 			ANIM.connect("animation_finished", Callable(self, "_on_animation_finished"))
-	
-func _on_animation_finished(_anim_name: String) -> void:
-	queue_free()	
+			queued_free = true
 
 func _ready() -> void:
 	REAPER = get_tree().root.get_node("Main/Reaper")
--- project/scripts/portal.gd
+++ project/scripts/portal.gd
@@ -1,10 +1,12 @@
 extends Node3D
 @export var AREA: Area3D
 @export var CUTSCENE_PLAYER: AnimationPlayer
+@export var ANIM: AnimationPlayer
 @export var REAPER: CharacterBody3D
 @export var PLAYER_SPOT: Node3D
 
 func _play_end_cutscene()-> void:
+	print('test')
 	REAPER.global_transform = PLAYER_SPOT.global_transform
 	REAPER.global_transform = PLAYER_SPOT.global_transform
 	REAPER.MESH.transform = Transform3D.IDENTITY
@@ -14,6 +16,7 @@ func _play_end_cutscene()-> void:
 func _on_body_entered(body: Node) -> void:
 	if not body == REAPER: return
 	REAPER.global_transform.origin = PLAYER_SPOT.global_transform.origin
+	ANIM.play("ESCAPE")
 	CUTSCENE_PLAYER.play("ESCAPE")
 	
 func _ready() -> void:
